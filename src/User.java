b0c119d4c3c9abe2f01cef506f5e0be97d3beec5
tahirdotz <tahirzaman075@gmail.com>
2024-11-15 23:43:00 +0600

    User class made

---
+++ src/User.java
@@ -0,0 +1,67 @@
+public class User {
+    private String username;
+    private String emailID;
+    private String password;
+    private String userID;
+    private Usertype usertype;
+
+    public User(String username, String emailID, String password, Usertype usertype)
+    {
+        this.username = username;
+        this.emailID = emailID;
+        this.password = password;
+        this.userID = username;
+        this.usertype = usertype;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Usertype getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(Usertype usertype) {
+        this.usertype = usertype;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", emailID='" + emailID + '\'' +
+                ", password='" + password + '\'' +
+                ", userID='" + userID + '\'' +
+                ", usertype=" + usertype +
+                '}';
+    }
+}
