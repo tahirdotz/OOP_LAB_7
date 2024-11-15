3e741affc77afd9cfdb9bdec07c1e5c663d673d6
tahirdotz <tahirzaman075@gmail.com>
2024-11-16 00:09:56 +0600

    Added UserReader

---
+++ src/UserReader.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class UserReader {
+    private String fileName;
+    public UserReader(String fileName)
+    {
+        this.fileName = fileName;
+    }
+    public List<User>readUsers()
+    {
+        List<User> users = new ArrayList<>();
+        try
+        {
+            BufferedReader bufferedReader=new BufferedReader(new FileReader(fileName));
+            String line = null;
+            while (bufferedReader.readLine()!=null)
+            {
+                String[] details=line.split(",");
+                users.add(new User(details[0],details[1],details[2],details[3],UserType.details[4]))
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return users;
+    }
+}
